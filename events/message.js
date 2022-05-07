1de65634336ff4c82d1181339cac9ed31c17c6d2
Clemens <clemen2912@gmail.com>
2022-05-07 23:52:57 +0200

    Changed text and color

--- events/message.js
+++ events/message.js
@@ -8,9 +8,10 @@ module.exports = async (client, message) => {
     if (message.content === `<@${client.user.id}>` || message.content === `<@!${client.user.id}>`) {
 
     const embed = new Discord.MessageEmbed()
-    .setTitle('Did you ping me ??')
+    .setColor("#ff2050")
+    .setTitle('Hello there did you ping me ??')
     .setThumbnail(client.user.displayAvatarURL({dynamic: true, size: 1024}))
-    .setDescription(`Aight I'm ${client.user.username} I'm a multi purpose bot my prefix is \`${config.prefix}\` try \`${config.prefix}help\` to get the list of all my commands.\n\n`)
+    .setDescription(`Aight I'm ${client.user.username} I'm the bot from <@564740301600718859> and my prefix is \`${config.prefix}\` try \`${config.prefix}help\` to get the list of all my commands.\n\n`)
     .setTimestamp()
     
     message.channel.send(embed)
