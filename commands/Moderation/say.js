85d4bf400c6205349f9ea32590d814bcb56c3a7b
Clemens <clemen2912@gmail.com>
2022-05-08 01:20:43 +0200

    change

---
+++ commands/Moderation/say.js
@@ -0,0 +1,30 @@
+const config = require('../../config.json');
+const { Client, Message, MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "say",
+  aliases: ["tell", "embed"],
+  category: "Moderation",
+  description: "Send message with bot in embed",
+  example: `${config.prefix}say [message]`,
+
+  run: async (client, message, args) => {
+
+    const perms = ["ADMINISTRATOR"]
+
+    if (!message.member.hasPermission(perms)) {
+        return message.reply(`❌ You can't send text with the bot.. since you don't have permission to do it ask admin or owner to give you permission **\`ADMINISTRATOR\`** !!`)
+        .then(msg => {
+            msg.delete({ timeout: 15000 })
+          })
+    }
+
+    const Embed = new MessageEmbed()
+        .setAuthor(message.author.tag, message.author.displayAvatarURL({ dyanmic: true }))
+        .setDescription(args.join(" "))
+        .setTimestamp()
+        .setColor("#ff2050")
+    message.delete()
+    message.channel.send(Embed)
+  },
+};
